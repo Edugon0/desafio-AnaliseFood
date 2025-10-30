0930d037e65e0188a4f6ce853d5fc81bd0de5f8f
Edugon0 <eduardo.vic.goncalves@gmail.com>
2025-10-30 15:42:20 -0300

    first commit

---
+++ backend/db.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DB_USER = "challenge"
+DB_PASSWORD = "challenge_2024"
+DB_HOST = "godlevel-db"
+DB_PORT= "5432"
+DB_NAME= "challenge_db"
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
